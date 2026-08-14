cb01f0f464b3c0cf34dbd4ba3e14caf74a0b1ba3
dorian <civond@users.noreply.github.com>
2026-08-13 19:04:29 -1000

    update logic

--- main.py
+++ main.py
@@ -1,130 +1,132 @@
 import cv2
 import numpy as np
-import rasterio
 import os
-import matplotlib.pyplot as plt
 import pandas as pd
-import toml
 
 from utils.load_tif import *
 from utils.load_mask import *
 from utils.tile_img import *
 from utils.percentile_stretch import *
 
-data_dir = "./data/"
+tile_size = 1024
+output_directory = "./output/"
+raster_directories = [
+    r"C:\Users\Dorian\Desktop\Storage\Work\Madin Lab\Halo Project Satellite Images\BLZ02a"
+]
 
-for file in os.listdir(data_dir):
-    if file.endswith(".tif"):
-        temp_img_path = os.path.join(data_dir, file)        # Create temp path
-        temp_img_path = os.path.join(data_dir, "BLZ01.tif")
-        temp_shp_path = os.path.join(data_dir, "LabeledHalos_BLZ01.shp")  
-        
-        # Load and Process .shp
-        mask = load_mask(temp_img_path, temp_shp_path)
-        mask = (mask > 0).astype(np.uint8) * 255
-        cv2.imwrite(f"test_dmask.png", mask)
-        #print(np.max(mask))
-        #print(mask.dtype)
+for directory in raster_directories:
+    contents = os.listdir(directory)
+    tif_name = directory.split("\\")[-1]
+    tif_path = None
+    shp_path = None
 
-        # Load and Process .tif
-        [img, profile] = load_tif(temp_img_path)            # Load .tif
-        img = np.transpose(img, (1, 2, 0))              # Reshape from (C,H,W) -> (H,W,C)
-        max_vals = np.max(img, axis=(0, 1))
-        print(f"\tChannel max vals: {max_vals}")
-        means = np.mean(img, axis=(0, 1))
-        print("\tChannel means:", means)
-
-        assert img.shape[:2] == mask.shape
-
-        RGB_IDX = [0, 1, 2]   # or [2,1,0], etc
-        NIR_IDX = 3
-        img = img[:, :, RGB_IDX].astype(np.float32)
-        img = np.where(img == 0, np.nan, img)
-
-        # Perform percentile stretch
-        for i in range(3):
-            channel = img[:, :, i]
-            stretched_channel = percentile_stretch(channel)
-            img[:, :, i] = stretched_channel
-
-        # Convert invalid values to int
-        img = np.nan_to_num(
-            img, 
-            nan=0.0, 
-            posinf=0.0, 
-            neginf=0.0
-        )
-        
-        # Normalize to 255 and performing tiling
-        tile_size = 512
-        img = (img * 255).astype(np.uint8)
-        [img_tiles, coords, dims] = tile_img(
-            img = img,
-            tile_size = tile_size
-        )
-        [mask_tiles, _, _] = tile_img(
-            img = mask,
-            tile_size = tile_size
-        )
-
-
-        # Create output directory
-        out_dir = "./output"
-        img_dir = os.path.join(out_dir, "img")
-        mask_dir = os.path.join(out_dir, "mask")
-        os.makedirs(img_dir, exist_ok=True)
-        os.makedirs(mask_dir, exist_ok=True)
-        
-        filename_array = []
-        img_path_array = []
-        mask_path_array = []
-
-        halo_present_array = []
-        coords_array = []
-
-        # Loop through tiles
-        for i, img_tile in enumerate(img_tiles):
-            temp_file_name = f"{file.split('.')[0]}_{i}.png"
-            temp_img_path = os.path.join(img_dir, temp_file_name)
-            temp_mask_path = os.path.join(mask_dir, temp_file_name)
-
-            # Show only valid tiles
-            if (np.mean(img_tile > 10) > 0.001):
-                filename_array.append(temp_file_name)
-                img_path_array.append(temp_img_path)
-                mask_path_array.append(temp_mask_path)
-                coords_array.append(coords[i])
-                #print(temp_img_path)
-                #print(temp_mask_path)
-                mask_tile = mask_tiles[i]
-
-                # If object present in mask
-                if np.max(mask_tile) == 255:
-                    halo_present_array.append(True)
-                else:
-                    halo_present_array.append(False)
-
-                cv2.imwrite(temp_img_path, img_tile)
-                cv2.imwrite(temp_mask_path, mask_tile)
-                #cv2.imshow(temp_mask_path, mask_tile)
-                #cv2.imshow(temp_img_path, img_tile)
-                #cv2.waitKey(0)
-                #cv2.destroyAllWindows()
-        df = pd.DataFrame({
-            "filename": filename_array,
-            "img_path": img_path_array,
-            "mask_path": mask_path_array,
-            "halo_present": halo_present_array,
-            "coord": coords_array
-        })
-
-        print(f"Writing .csv")
-        df.to_csv("dataset.csv", index=False)
-        val_counts = df["halo_present"].value_counts()
-        print(val_counts)
-
-        #cv2.imwrite(f"test.png", img)
+    # Populate .tif and .shp filepaths
+    for filename in contents:
+        if filename.endswith(".tif"):
+            tif_path = os.path.join(directory, filename)
+        elif filename.endswith(".shp"):
+            shp_path = os.path.join(directory, filename)
     
+    if tif_path is None or shp_path is None:
+        print(f"Warning: missing .tif or .shp in {directory}")
+        continue
 
-        
-        input('pause')
+    # Load and Process .shp
+    mask = load_mask(tif_path, shp_path)
+    mask = (mask > 0).astype(np.uint8) * 255
+    cv2.imwrite(f"test_dmask.png", mask)
+
+    # Load and Process .tif
+    [img, profile] = load_tif(tif_path)            # Load .tif
+    img = np.transpose(img, (1, 2, 0))                        # Reshape from (C,H,W) -> (H,W,C)
+    max_vals = np.max(img, axis=(0, 1))
+    print(f"\tChannel max vals: {max_vals}")
+    means = np.mean(img, axis=(0, 1))
+    print("\tChannel means:", means)
+
+    assert img.shape[:2] == mask.shape
+
+    RGB_IDX = [0, 1, 2]   # or [2,1,0], etc
+    NIR_IDX = 3
+    img = img[:, :, RGB_IDX].astype(np.float32)
+    img = np.where(img == 0, np.nan, img)
+
+    # Perform percentile stretch
+    for i in range(3):
+        channel = img[:, :, i]
+        stretched_channel = percentile_stretch(channel)
+        img[:, :, i] = stretched_channel
+
+    # Convert invalid values to int
+    img = np.nan_to_num(
+        img, 
+        nan=0.0, 
+        posinf=0.0, 
+        neginf=0.0
+    )
+    
+    # Normalize to 255 and performing tiling
+    img = (img * 255).astype(np.uint8)
+    [img_tiles, coords, dims] = tile_img(
+        img = img,
+        tile_size = tile_size
+    )
+    [mask_tiles, _, _] = tile_img(
+        img = mask,
+        tile_size = tile_size
+    )
+
+
+    # Create output directory
+    img_write_dir = os.path.join(output_directory, "img")
+    mask_write_dir = os.path.join(output_directory, "mask")
+    os.makedirs(img_write_dir, exist_ok=True)
+    os.makedirs(mask_write_dir, exist_ok=True)
+    
+    filename_array = []
+    img_path_array = []
+    mask_path_array = []
+
+    halo_present_array = []
+    coords_array = []
+
+    # Loop through tiles
+    for i, img_tile in enumerate(img_tiles):
+        temp_tile_name = f"{tif_name}_{i}.png"
+        temp_img_path = os.path.join(img_write_dir, temp_tile_name)
+        temp_mask_path = os.path.join(mask_write_dir, temp_tile_name)
+
+        # Show only valid tiles
+        if (np.mean(img_tile > 10) > 0.001):
+            filename_array.append(temp_tile_name)
+            img_path_array.append(temp_img_path)
+            mask_path_array.append(temp_mask_path)
+            coords_array.append(coords[i])
+            #print(temp_img_path)
+            #print(temp_mask_path)
+            mask_tile = mask_tiles[i]
+
+            # If object present in mask
+            if np.max(mask_tile) == 255:
+                halo_present_array.append(True)
+            else:
+                halo_present_array.append(False)
+
+            print(f"\tWriting to: {temp_img_path}")
+            cv2.imwrite(temp_img_path, img_tile)
+            cv2.imwrite(temp_mask_path, mask_tile)
+
+    df = pd.DataFrame({
+        "filename": filename_array,
+        "img_path": img_path_array,
+        "mask_path": mask_path_array,
+        "coord": coords_array,
+        "tile_size": tile_size,
+        "halo_present": halo_present_array,
+    })
+
+    print(f"Writing .csv")
+    temp_dataset_path = os.path.join(output_directory, f"{tif_name}_dataset.csv")
+    df.to_csv(f"{tif_name}_dataset.csv", index=False)
+    val_counts = df["halo_present"].value_counts()
+    print(val_counts)
